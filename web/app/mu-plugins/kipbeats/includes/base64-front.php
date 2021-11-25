7dda127b09ba29e9d97b6d4f1faa16a35f5a72d4
kipapps <ki2thap@gmail.com>
2021-11-25 00:24:06 -0500

    Add files via upload

--- web/app/mu-plugins/kipbeats/includes/base64-front.php
+++ web/app/mu-plugins/kipbeats/includes/base64-front.php
@@ -1,6 +1,6 @@
 <?php
 
-add_filter( 'post_thumbnail_html', function ( $html ) {
+/*add_filter( 'post_thumbnail_html', function ( $html ) {
     if (!$html) {return $html;}
     $id = get_post_thumbnail_id();
     $dom = new DOMDocument();$dom->loadXML($html);
@@ -14,7 +14,23 @@ add_filter( 'post_thumbnail_html', function ( $html ) {
     }
     //$img->setAttribute('src','Test IMG');
     return $html;
-});
+});*/
+function check_image_url( $image, $attachment_id, $size, $icon ){
+    $id = $attachment_id;$src = $image[0];
+    $base64 = ( get_post_meta( $id, '_wp_attachment_base64', true ) ? get_post_meta( $id, '_wp_attachment_base64', true ) : null);
+    $file = WP_CONTENT_DIR . '/uploads/'. get_post_meta( $id, '_wp_attached_file' , true );
+    $tfile = dirname($file).'/'.basename($src);
+    if ( ! is_file($tfile) && $base64) {
+        if (! is_file($file)) {file_put_contents( $file, base64_decode( explode(';base64,', $base64)[1] ) );}
+        $editor = wp_get_image_editor( $file, array() );
+        $result = $editor->resize($image[1],$image[2], true);
+        $editor->save($editor->generate_filename());
+        
+    }
+    return $image;
+}
+add_filter( 'wp_get_attachment_image_src', 'check_image_url', 10, 4 );
+
 
 
 ?>
